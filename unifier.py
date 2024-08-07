89bc526c6e30616300d16f54e1cb05c3218e1766
Arhznblaster <128213818+Arhznblaster@users.noreply.github.com>
2024-08-07 13:47:32 +0600

    Update unifier.py

--- unifier.py
+++ unifier.py
@@ -38,7 +38,7 @@ if os.name != "nt":
         pass
 
 try:
-    with open('config.json', 'r') as file:
+    with open('config.toml', 'r') as file:
         data = toml.load(file)
 except:
     traceback.print_exc()
